3060aaa7a039808c6329194bc50ca7cceb1a23c0
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-08-23 00:34:39 -0300

    criação de Navbar com bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Rotas from './components/Rotas'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar/>
     <Rotas/>
   </StrictMode>,
 )
