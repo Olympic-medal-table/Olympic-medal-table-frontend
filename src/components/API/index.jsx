62d7e038cf9e77a5a5739a13144e8dd126686b5b
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-08-24 20:32:58 -0300

    alterado endereço das requests

--- src/components/API/index.jsx
+++ src/components/API/index.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'http://localhost:8080/'
+    //alterar para o endereço do gateway da sua máquina
+    baseURL:'http://192.168.0.104:8082/olympics-medal-table-ms'
 });
 export default API;
